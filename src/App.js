28f93acfab28d423ecfe1708a07fbce6aaa891e4
GowthamaViknesh <gowthampostbox18@gmail.com>
2023-08-11 14:31:23 +0530

    Url-frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        'https://url-dzu5.onrender.com/shorturl',
+        {
+          url: url,
+        }
+      );
+
+      if (response.data.shortURL) {
+        setShortUrl(response.data.shortURL);
+        setUrl('');
+      }
+    } catch (error) {
+      console.error('Error creating short URL:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div>
+        <h1 className='center-text'>URL SHORTENER</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='url'
+            id='url'
+            placeholder='Url Here...'
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button className='btn btn-primary mr-1 ml-1' type='submit'>
+            Magic
+          </button>
+        </form>
+        <div className='mt-4 '>
+          {shortUrl && (
+            <div>
+              <p>Short URL:</p>
+              <a href={shortUrl} target='_blank' rel='noopener noreferrer'>
+                {shortUrl}
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
